adb2f0083e2d74ab72db855589c66f03b5478b10
Brago <bragolab@gmail.com>
2017-10-20 13:00:33 +0200

    - Aggiunto script MouseOverAnimation.

---
+++ Assets/_Project/Script/MouseInteraction.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteraction : MonoBehaviour {
+
+    public bool Unbreackable;
+
+    public int Speed = 10;
+    public string ModelName = "";
+    public bool IsSold = false;
+    public float Float = 0.454f;
+
+    private bool HaStampato;
+
+    private void OnEnable() {
+        
+    }
+
+    private void Update() {
+        Float = Float + 0.002f;
+    }
+
+    // Use this for initialization
+    void Start() {
+        HaStampato = false;
+        HaStampato = StampaDati();
+        Debug.Log("Risultato della stampa: " + HaStampato);
+    }
+
+    private void OnMouseOver() {
+        if (Unbreackable == true) {
+            return;
+        }
+        Debug.Log("Interaction");
+        Destroy(gameObject);
+    }
+
+    bool StampaDati() {
+        if (ModelName == "") {
+            return false;
+        }
+        Debug.Log("Auto " + ModelName + " Speed: " + Speed);
+        return true;
+    }
+
+    bool Limitatore() {
+        if (Speed > 50 && Speed < 70) {
+            return true;
+        } else if (Speed < 0 || Speed > 100000) {
+            return true;
+        } else {
+
+        }
+        return false;
+    }
+
+    float Accellera() {
+        if (Limitatore() == true) {
+            Speed = Speed + 1;
+        }
+       
+        return Speed;
+    }
+
+}
---
+++ Assets/_Project/Script/MouseOverAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MouseOverAnimation : MonoBehaviour {
+
+    [Tooltip("Indicare il nome parametro booleano dell'animation controller che controlla l'animazione all'OnMouseOver.")]
+    public string OverAnimationParameterName;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver() {
+        anim.SetBool("Open", true);
+    }
+
+    private void OnMouseExit() {
+        anim.SetBool("Open", false);
+    }
+}
