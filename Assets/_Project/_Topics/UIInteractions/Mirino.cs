322a7f4a29de1abc5eb981cf6b19397feff43beb
Brago <bragolab@gmail.com>
2018-01-10 10:40:03 +0100

    - UI interactions

---
+++ Assets/_Project/_Topics/UIInteractions/Mirino.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirino : MonoBehaviour {
+    Camera cam;
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            print("Sto puntando a " + hit.transform.name);
+        else
+            print("Non sto puntando a niente!");
+    }
+}
